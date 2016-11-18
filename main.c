1b11ef8f3be3842fa9ccb3cfaa814910ae7825fb
Lucas Alegre <lucasnale@hotmail.com>
2016-11-18 16:43:52 -0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
 UFRGS - INF 01203 – Estruturas de Dados
 Trabalho Final 2016/2
-Comparação	do	Desempenho	de	Árvores Binárias
+Comparação do Desempenho de	Árvores Binárias
 ABP X AVL
 
 Alunos:
@@ -18,24 +18,28 @@ Guilherme Gomes Haetinger
 #include "abp.h"
 #include "avl.h"
 
-/* Exemplo de uso dos argumentos para main
-O programa l� um arquivo texto como entrada e gera um arquivo como sa�da com o conte�do do arquivo de entrada convertido para letras mai�sculas
-Para chamar, digite "exemplo entrada.txt saida.txt" */
-
 int main(int argc, char *argv[]) //argc conta o n�mero de par�metros e argv armazena as strings correspondentes aos par�mentros digitados
 {
-
     setlocale(LC_ALL,""); //para imprimir corretamente na tela os caracteres acentuados
-    clock_t start, end, elapsed; //para contar o tempo
+
+    pNodoABP* abp = inicializa_ABP();
+    pNodoAVL* avl = inicializa_AVL();
+
+    clock_t start, end, elapsed; //para contar o tempo DOUBLE
+
     int lineno=0;
+    int nodo;
+    int op;
 
-    FILE * entrada;
-    FILE * saida;
+    FILE* roteiro;
+    FILE* nodos;
 
+    char nomeArq[20];
+    char operacao;
     char *palavra, linha[1000]; // linhas a serem lidas do arquivo
     char separador[]= {" 0123456789,.&*%\?!;/-'@\"$#=><()][}{:\n\t"};
 
-    if (argc != 3)  //o numero de parametros esperado � 3: nome do programa (argv[0]), nome do arq de entrada(argv[1]), nome do arq de saida(argv[2])
+    if (argc != 2)  //o numero de parametros esperado � 3: nome do programa (argv[0]), nome do arq de entrada(argv[1]), nome do arq de saida(argv[2])
     {
         printf ("N�mero incorreto de par�metros.\n Para chamar o programa digite: exemplo <arq_entrada> <arq_saida>\n");
         return 1;
@@ -43,35 +47,73 @@ int main(int argc, char *argv[]) //argc conta o n�mero de par�metros e argv 
     else
     {
 
-        entrada = fopen (argv[1], "r"); // abre o arquivo para leitura -- argv[1] � o primeiro par�metro ap�s o nome do arquivo.
-        if (entrada == NULL) //se n�o conseguiu abrir o arquivo
-        {
-            printf ("Erro ao abrir o arquivo %s",argv[1]);
-            return 1;
-        }
-        else // arquivo de entrada OK
+        do{
+            printf("Executar para ABP ou AVL? (ABP - 1, AVL - 2)\n");
+            scanf("%d", &op);
+        }while(op != 1 && op != 2);
 
+        if(op == 1)  //ABP
         {
-            saida = fopen (argv[2], "w"); // abre o arquivo para saida -- argv[2] � o segundo par�metro ap�s o nome do arquivo.
-            start=clock(); //inicia a contagem do tempo;
 
-            //percorre todo o arquivo lendo linha por linha
-            while (fgets(linha,1000,entrada))
+            roteiro = fopen(argv[1], "r"); // abre o arquivo para leitura -- argv[1] � o primeiro par�metro ap�s o nome do arquivo.
+            if (roteiro == NULL) //se n�o conseguiu abrir o arquivo
             {
-                palavra = strtok (linha, separador); //considera qquer caractere n�o alfab�tico como separador
-                while (palavra != NULL)
-                {
-                    fprintf(saida," %s", strlwr(palavra)); //strlwr � a fun��o que converte palavras para min�sculo
-                    palavra = strtok (NULL, separador);
-                }
+                printf ("Erro ao abrir o arquivo %s",argv[1]);
+                return 1;
             }
-            end=clock(); //l� o tempo final
-            elapsed = 1000 * (end - start) / (CLOCKS_PER_SEC); //calcula o tempo decorrido em milissegundos
-            printf("\nO tempo gasto no processamento do arquivo %s foi de %ld ms\n",argv[1],elapsed);
-            printf("\nArquivo %s gerado com sucesso.\n",argv[2]);
+            printf("******VERSÃO COM ABP*********\n");
+            fscanf(roteiro,"%c",&operacao);
+            operacao = toupper(operacao);
+            switch(operacao)
+            {
+            case 'I':
+                     fgets(nomeArq,20,roteiro);
+                     nodos = fopen(nomeArq,"r");
+                     if (nodos == NULL) //se n�o conseguiu abrir o arquivo
+                     {
+                        printf ("Erro ao abrir o arquivo %s", nomeArq);
+                        return 1;
+                     }
+                     printf("Inserindo dados do arquivo %s\n", nomeArq);
+                     while(nodos != NULL)
+                     {
+                         fscanf(nodos,"%d",&nodo);
+                         abp = insere_arvore(abp, nodo);
+                     }
+
+                     break;
+            case 'R':
+                     fgets(nomeArq,20,roteiro);
+                     nodos = fopen(nomeArq,"r");
+                     if (nodos == NULL) //se n�o conseguiu abrir o arquivo
+                     {
+                        printf ("Erro ao abrir o arquivo %s", nomeArq);
+                        return 1;
+                     }
+                     printf("Removendo dados do arquivo %s\n", nomeArq);
+
+                     break;
+            case 'C':
+                     fgets(nomeArq,20,roteiro);
+                     nodos = fopen(nomeArq,"r");
+                     if (nodos == NULL) //se n�o conseguiu abrir o arquivo
+                     {
+                        printf ("Erro ao abrir o arquivo %s", nomeArq);
+                        return 1;
+                     }
+                     printf("Consultando dados do arquivo %s\n", nomeArq);
+
+                     break;
+            case 'E':
+                     printf("*****ESTATÍSTICAS ABP******\n");
+
+                     break;
+            }
+
+
         }
-        fclose (entrada); //fecha os arquivos
-        fclose (saida);
-        return 0;
     }
+
+    return 0;
+
 }
